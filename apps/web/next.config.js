66daa4338a816db6dd05f2c0655be2ae63f0bbcc
Debashish Patra <hi@suvam.io>
2024-06-29 10:35:29 +0530

    chore: remove image config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// image optimize
-	images: {
-		unoptimized: true,
-		formats: ['image/webp'],
-		disableStaticImages: true,
-	},
+	// images: {
+	// 	unoptimized: true,
+	// 	formats: ['image/webp'],
+	// 	disableStaticImages: true,
+	// },
 };
 
 module.exports = nextConfig;
