e65acced30237be766be07f5acfc4f6cddfdde5a
Debashish Patra <hi@suvam.io>
2024-08-11 03:42:43 +0530

    chore: experimental pnpm support

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,10 +1,12 @@
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
+import "expo-router/entry.js"
 
-// Must be exported or Fast Refresh won't update the context
-export function App() {
-	const ctx = require.context('./app');
-	return <ExpoRoot context={ctx} />;
-}
-
-registerRootComponent(App);
+// import { registerRootComponent } from 'expo';
+// import { ExpoRoot } from 'expo-router';
+//
+// // Must be exported or Fast Refresh won't update the context
+// export function App() {
+// 	const ctx = require.context('./app');
+// 	return <ExpoRoot context={ctx} />;
+// }
+//
+// registerRootComponent(App);
