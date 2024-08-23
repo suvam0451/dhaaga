32ac7fd2522006c5bbf89f4f4c7e6970804086e7
Debashish Patra <hi@suvam.io>
2024-08-23 23:05:44 +0530

    chore: version bumps

--- apps/web/src/app/components/custom/Buttons.tsx
+++ apps/web/src/app/components/custom/Buttons.tsx
@@ -10,7 +10,7 @@ export function GithubDownloadButton() {
 	return (
 		<a
 			href={
-				'https://github.com/suvam0451/dhaaga/releases/download/v0.7.0/Dhaaga-v0.7.0.apk'
+				'https://github.com/suvam0451/dhaaga/releases/download/v0.8.0/Dhaaga-v0.8.0.apk'
 			}
 		>
 			<Box
