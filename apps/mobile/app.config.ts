211e00961276ef09ed3d24b567d7c681789efcf1
Debashish Patra <hi@suvam.io>
2024-08-23 16:26:51 +0530

    chore: version bump

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -6,7 +6,7 @@ const expo = ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
 	name: IS_DEV ? 'Dhaaga (Dev)' : 'Dhaaga',
 	slug: 'dhaaga',
-	version: '0.7.0',
+	version: '0.8.0',
 	orientation: 'portrait',
 	icon: './assets/placeholder_icon.png',
 	userInterfaceStyle: 'dark',
@@ -21,7 +21,7 @@ const expo = ({ config }: ConfigContext): ExpoConfig => ({
 	},
 	android: {
 		package: IS_DEV ? 'io.suvam.dhaaga.dev' : 'io.suvam.dhaaga',
-		versionCode: 9,
+		versionCode: 10,
 	},
 	androidStatusBar: {
 		barStyle: 'dark-content',
