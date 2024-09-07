151de3f2d31787eb009fb306ce8503d855a15c65
Debashish Patra <hi@suvam.io>
2024-09-07 22:57:39 +0530

    chore: version/notes bump

--- apps/web/src/app/components/Introduction.tsx
+++ apps/web/src/app/components/Introduction.tsx
@@ -99,7 +99,7 @@ function Introduction() {
 							marginTop: 8,
 						}}
 					>
-						v0.8.0 • Play store build is available as closed beta.
+						v0.10.0 • Play store build is available as closed beta.
 					</p>
 				</Box>
 
