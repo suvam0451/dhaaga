8f8c99fb771c2f3c511804123b6996d3c226fe74
Debashish Patra <hi@suvam.io>
2024-11-11 08:42:13 +0530

    chore: expo 51 change lock

--- apps/mobile/states/global/atomic.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'jotai';
-import { randomUUID } from 'crypto';
-
-export const isAppBottomSheetVisible = atom(false);
-export const isNativeBottomSheetVisible = atom(false);
-
-export const appBottomSheetRequestId = atom(randomUUID());
