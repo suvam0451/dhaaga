1bc7b7b0f850667e889fe3db7d3ca6889ecc1632
Debashish Patra <hi@suvam.io>
2025-09-04 22:53:17 +0530

    chore: remove desktop app codebase

--- apps/desktop/pkg/threadsapi/service_thread_private.go
+++
@@ -1,8 +0,0 @@
-package threadsapi
-
-type ThreadsApi_Private_TextFeed_Profile struct {
-	// medias []ThreadsApi_Media `json:"medias"`
-	Threads   []ThreadsApi_Thread `json:"threads"`
-	NextMaxId string              `json:"next_max_id"`
-	Status    string              `json:"status"`
-}
--- apps/desktop/pkg/utils/auth.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-type ThreadsDb_Account_Create_DTO struct {
-	Domain    string `json:"domain" db:"domain"`
-	Subdomain string `json:"subdomain" db:"subdomain"`
-	Username  string `json:"username" db:"username"`
-	Password  string `json:"password" db:"password"`
-	Verified  bool   `json:"verified" db:"verified"`
-}
