22ddaca9c1896ae2205c3a73bc63e113d3d92a5c
Debashish Patra <suvam0451@icloud.com>
2025-12-22 10:06:06 +0530

    fix: relative path

--- apps/mobile/scripts/expo-build-pre-install.js
+++ apps/mobile/scripts/expo-build-pre-install.js
@@ -1,6 +1,6 @@
 import { execSync } from 'node:child_process';
 import stripLicensedAssetUse from './strip-licensed-asset-use.js';
-import linkAssets from '#/scripts/link-assets.js';
+import linkAssets from './link-assets.js';
 
 function run(cmd) {
 	execSync(cmd, { stdio: 'inherit' });
