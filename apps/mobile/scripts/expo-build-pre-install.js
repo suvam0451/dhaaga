48ad83234f69f6a6e6a1074a778f0f5ffe382f2b
Debashish Patra <suvam0451@icloud.com>
2025-12-22 10:13:20 +0530

    fix: recursive fn call

--- apps/mobile/scripts/expo-build-pre-install.js
+++ apps/mobile/scripts/expo-build-pre-install.js
@@ -1,12 +1,5 @@
-import { execSync } from 'node:child_process';
 import stripLicensedAssetUse from './strip-licensed-asset-use.js';
 import linkAssets from './link-assets.js';
-
-function run(cmd) {
-	execSync(cmd, { stdio: 'inherit' });
-}
-
-run('npm run eas-build-pre-install');
 
 linkAssets();
 stripLicensedAssetUse();
