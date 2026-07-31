c1151c87328f9c773f24a005ec73404d7d826d73
Debashish Patra <suvam0451@icloud.com>
2026-07-31 18:30:32 +0530

    chore: failing tl check

--- packages/scripts/check-i18n.js
+++ packages/scripts/check-i18n.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const localesDir = path.join(__dirname, '../apps/mobile/i18n/locales');
+const localesDir = path.join(__dirname, '../../apps/mobile/i18n/locales');
 const enDir = path.join(localesDir, 'en');
 
 function getKeys(obj, prefix = '') {
